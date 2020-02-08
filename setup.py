b55ea53bc9b50c04666f9cc347d66239a816a731
Kyle Barron <barronk@mit.edu>
2020-02-08 12:03:38 -0500

    Add setup script

---
+++ setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('CHANGELOG.md') as history_file:
+    history = history_file.read()
+
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.readlines()
+    requirements = [x[:-1] for x in requirements]
+
+with open('requirements_dev.txt') as test_requirements_file:
+    test_requirements = test_requirements_file.readlines()
+    test_requirements = [x[:-1] for x in test_requirements]
+
+setup_requirements = ['setuptools >= 38.6.0', 'twine >= 1.11.0']
+
+# yapf: disable
+setup(
+    author="Kyle Barron",
+    author_email='kylebarron2@gmail.com',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Wrapper around transit.land API",
+    # entry_points={
+    #     'console_scripts': [
+    #         'demquery=demquery.cli:main',
+    #     ],
+    # },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    keywords='transitland_wrapper',
+    name='transitland_wrapper',
+    packages=find_packages(include=['transitland_wrapper', 'transitland_wrapper.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/kylebarron/transitland_wrapper',
+    version='0.1.0',
+    zip_safe=False,
+)
